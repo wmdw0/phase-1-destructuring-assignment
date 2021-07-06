676c5a58df569b3116685c061776a916b6654a77
William Walsh <wmdwalsh0@gmail.com>
2021-07-05 23:24:00 -0400

    commit tree 5c71436fd8546b8bd2270fbf68788e701933d2bf

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite, ,  black, pink, ] = farmAnimals.split(' ');
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, indg, v] = colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,7 +14,7 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
-
+const { muppetName, color, song, job, partner } = muppet;
 const nestedMuppet = {
   nestedName: 'Kermit',
   nestedColor: 'green',
@@ -24,6 +29,8 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const { song2, song4 } = nestedMuppet.album.theMuppetMovie;
+const { nestedJob, nestedPartner } = nestedMuppet;
 
 // Strings
 
